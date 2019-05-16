7282107be09d7de4026932dcee4d4af4b86020ac
rober <rober@DESKTOP-G35UCGI>
2019-05-16 17:22:21 -0400

    editing statechanges again

--- MyWebProject/src/Authentication.java
+++ MyWebProject/src/Authentication.java
@@ -27,47 +27,25 @@ public class Authentication extends HttpServlet {
 	
 	public Authentication() {
 		Authentication.client_id = "iyn_zN-dKNdmrw";
-		
 		Authentication.redirect_url = "http://99.227.1.78:8080/MyWebProject/Login.html";
-		
 		Authentication.response_type = "code";
-<<<<<<< HEAD
-	}--
-	private String getUserName() {
-		return LoginServlet.username;
-	}
-	
-	private void checkUrl(String Url) throws IOException {
-		if (redirect_url != Url) {
-			throw new IOException();
-		}
-	}
-	
-	private String stateChanges() {
-=======
 		Authentication.scope = "history";
 		Authentication.duration = "temporary";
 	}
-	
 	private void stateChanges() {
->>>>>>> branch 'develop' of https://github.com/abohakobyan/MyWebProject.git
 		int n = 20;
 		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvwxyz";
 		StringBuilder sb = new StringBuilder(n);
-		for (int i = 19; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 		int index = (int) ((int) (AlphaNumericString.length()) * Math.random());
 		sb.append(AlphaNumericString.charAt(index));
 		}
 		Authentication.state = sb.toString();
 		
 }
-<<<<<<< HEAD
 	protected String createLink() {
-=======
-	public static String createLink() {
 		Authentication a = new Authentication();
 		a.stateChanges();
->>>>>>> branch 'develop' of https://github.com/abohakobyan/MyWebProject.git
 		link = "https://www.reddit.com/api/v1/authorize?client_id=" + client_id + "&response_type=" + response_type +
 				 "&state=" + state + "&redirect_uri=" + redirect_url + "&duration=" + duration + "&scope=" + scope;
 		return link;
