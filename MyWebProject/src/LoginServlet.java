c5df38efe88fc7edfecb40ce5be4d0052f270c71
aboha <aboha@Excalibur>
2019-05-16 13:25:31 -0400

    First Push

---
+++ MyWebProject/src/LoginServlet.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+ 
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+ 
+    	String number = request.getParameter("ND_DP");
+    	System.out.println("number: " + number);
+    	
+    	PrintWriter writer = response.getWriter();
+    	writer.println("there are korean bous");
+    }
+ 
+}
