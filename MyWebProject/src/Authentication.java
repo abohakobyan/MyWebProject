5def5ad6842ec0cd0e5e8e6ecfc11dff648adb13
aboha <aboha@Excalibur>
2019-05-16 17:14:07 -0400

    Authentication Success

--- MyWebProject/src/Authentication.java
+++ MyWebProject/src/Authentication.java
@@ -1,23 +1,22 @@
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 
-import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpContent;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.UrlEncodedContent;
 
-import com.google.appengine.api.utils.SystemProperty;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*; 
+public class Authentication extends HttpServlet {
 
-public class Authentication {
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static String response_type;
 	private static String state;
 	private static String redirect_url;
@@ -27,33 +26,29 @@ public class Authentication {
 	private static String scope;
 	
 	public Authentication() {
-		Authentication.client_id = "hkHbfsPB7zfGZdUKgSoOchEbB2I";
-		Authentication.redirect_url = "http://99.227.1.78:8080/MyWebProject/Box%20Portfolio/single-project.html";
+		Authentication.client_id = "iyn_zN-dKNdmrw";
+		
+		Authentication.redirect_url = "http://99.227.1.78:8080/MyWebProject/Login.html";
+		
 		Authentication.response_type = "code";
+		Authentication.scope = "history";
+		Authentication.duration = "temporary";
 	}
 	
-	private String getUserName() {
-		return LoginServlet.username;
-	}
-	
-	private void checkUrl(String Url) throws IOException {
-		if (redirect_url != Url) {
-			throw new IOException();
-		}
-	}
-	
-	private String stateChanges() {
+	private void stateChanges() {
 		int n = 20;
 		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvwxyz";
 		StringBuilder sb = new StringBuilder(n);
 		int index = (int) ((int) (AlphaNumericString.length()) * Math.random());
 		sb.append(AlphaNumericString.charAt(index));
 		Authentication.state = sb.toString();
-		return Authentication.state;
+		
 }
-	private String createLink() {
+	public static String createLink() {
+		Authentication a = new Authentication();
+		a.stateChanges();
 		link = "https://www.reddit.com/api/v1/authorize?client_id=" + client_id + "&response_type=" + response_type +
-				 "&state=" + state + "&redirect_ur=i" + redirect_url + "&duration=" + duration + "&scope=" + scope;
+				 "&state=" + state + "&redirect_uri=" + redirect_url + "&duration=" + duration + "&scope=" + scope;
 		return link;
 	}
 }
--- MyWebProject/src/LoginServlet.java
+++ MyWebProject/src/LoginServlet.java
@@ -6,10 +6,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
 
 
 @WebServlet("/LoginServlet")
-public class LoginServlet extends Authentication {
+public class LoginServlet extends Authentication{
  
     /**
 	 * 
@@ -21,10 +24,17 @@ public class LoginServlet extends Authentication {
 	
 	protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
+		String l = Authentication.createLink();
+		System.out.println(l);
+		response.setContentType("text/html");  
+		PrintWriter pw=response.getWriter();  
+		  
+		response.sendRedirect(l);  
+		  
+		pw.close();  
 		
-		PrintWriter writer = response.getWriter();
 		
-		
+		/*
     	username = request.getParameter("username");
     	String password = request.getParameter("password");
     	if(username != "" && password != "") {
@@ -32,6 +42,6 @@ public class LoginServlet extends Authentication {
     	}else {
     		writer.println("Please enter a valid username and password!");
     	}
-    	
-
+    	*/
+	}
 }
